cac650a8db3820430a078866f3d39a332b6aa6a7
MarkitoRosas <marcoantonio.rosasgonzalez@utxicotepec.edu.mx>
2023-10-09 18:04:08 -0600

    Actualizacion

---
+++ Backend/loadingpage/lib/main.dart
@@ -0,0 +1,25 @@
+// Creado por:
+// Asignatura: Desarrollo Movil Integral
+//Grado: 10   Grupo: "A"
+// Docente: MTI. Marco Antonio Ramirez Hernandez
+import 'package:flutter/material.dart';
+import 'package:loadingpage/home.dart'; // Importa el archivo home.dart
+import 'package:loadingpage/splash_screen.dart'; // Importa el archivo splash_screen.dart
+
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashScreen(), // Inicia con la pantalla de splash
+    theme: new ThemeData.dark(),
+  ));
+
+  // Agregar un Future.delayed para esperar un tiempo antes de navegar a la pantalla principal
+  Future.delayed(Duration(seconds: 25), () {
+    runApp(new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:
+          Home(), // Navega a la pantalla principal después de 3 segundos (ajusta el tiempo según sea necesario)
+      theme: new ThemeData.dark(),
+    ));
+  });
+}
